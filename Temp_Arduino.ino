565bf07129e8e98beb79920f7c3a471d7962bd18
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-24 12:49:42 -0600

    Mostrar la temperatura en el display por i2C

--- Temp_Arduino.ino
+++ Temp_Arduino.ino
@@ -1,13 +1,30 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup() {
   lcd.init();                      // initialize the lcd 
   lcd.backlight();
+   Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("Adafruit MLX90614 test");
+
+  if (!mlx.begin()) {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
 }
 
 void loop() {
-  lcd.setCursor(3,0);
-  lcd.print("Hello, world!");
+
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  lcd.setCursor(1,0);
+  lcd.print(mlx.readObjectTempC());
+  Serial.println();
+  delay(3000);
+  
 }
