5fbd7b38a17cc14fe1ac6859edc7e43865cc8a1f
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-24 12:42:14 -0600

    Update Temp_Arduino.ino
    Hello World LCD I2C ARDUINO DISPLAY

--- Temp_Arduino.ino
+++ Temp_Arduino.ino
@@ -1,10 +1,13 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup() {
-  // put your setup code here, to run once:
-
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  lcd.setCursor(3,0);
+  lcd.print("Hello, world!");
 }
