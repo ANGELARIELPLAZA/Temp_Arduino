4f2a3e9fcb3ca49e31fb03b59c858fb988a55a3b
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-24 14:46:34 -0600

    Se agrego sensor ultrasonico

--- Temp_Arduino.ino
+++ Temp_Arduino.ino
@@ -1,6 +1,10 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Adafruit_MLX90614.h>
+#include <Ultrasonic.h>
+
+Ultrasonic ultrasonic(12, 13);
+int distance;
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
@@ -21,10 +25,18 @@ void setup() {
 
 void loop() {
 
-  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
-  lcd.setCursor(1,0);
-  lcd.print(mlx.readObjectTempC());
+  Serial.print("Object = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  float TEMP = mlx.readObjectTempC(); 
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
+  lcd.print(TEMP,1);
+  lcd.print("\337C");
   Serial.println();
-  delay(3000);
+   // Pass INC as a parameter to get the distance in inches
+  distance = ultrasonic.read();
+  
+  Serial.print("Distance in CM: ");
+  Serial.println(distance);
+  delay(2000);
   
 }
